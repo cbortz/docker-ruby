d9fa3fac2a60a0de5e2cc49341fb115f5f4de739
Chet Bortz <chet@bortz.co>
2019-01-30 02:06:34 -0500

    Add subdirectory to Ruby download URL
    - Ruby 2.6 (coming soon!) is only available at proper subdirectory

--- 2.5/config.mk
+++ 2.5/config.mk
@@ -1,5 +1,6 @@
 export RUBY_VERSION = 2.5.3
 export RUBY_SHA1SUM = f919a9fbcdb7abecd887157b49833663c5c15fda
+export RUBY_SUBDIR = 2.5
 
 export RUBYGEMS_VERSION = 3.0.2
 export BUNDLER_VERSION = 2.0
