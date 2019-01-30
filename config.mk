a195a3f8da68a81945b01508c9f1bfad0a72f31e
Chet Bortz <chet@bortz.co>
2019-01-30 03:10:48 -0500

    Allow for additional ruby dependencies and config
    - RUBY_BUILD_DEPENDENCIES allows for one or more apt dependencies
    - RUBY_CONFIG_OPTS allows for passing one or more flags to configure
    - RUBY_TAG_SUFFIX will be injected into the tag name if present

--- config.mk
+++ config.mk
@@ -2,6 +2,10 @@ REGISTRY = quay.io
 REPOSITORY = cbortz/ruby
 
 export TAG = $(RUBY_TAG)-$(FROM_OS)-$(FROM_TAG)
+
+# Conditionally inject Ruby tag suffix, if present
+[ -z "$(RUBY_TAG_SUFFIX)" ] || export TAG = $(RUBY_TAG)-$(RUBY_TAG_SUFFIX)-$(FROM_OS)-$(FROM_TAG)
+
 export FROM = cbortz/$(FROM_OS):$(FROM_TAG)
 
 PUSH_TAGS = $(TAG)
