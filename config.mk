61f1908febbd745c09bbd7744b1d9ddda741328f
Chet Bortz <chet@bortz.co>
2019-01-30 03:10:01 -0500

    Remove `ruby-` tag alias

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ REPOSITORY = cbortz/ruby
 export TAG = $(RUBY_TAG)-$(FROM_OS)-$(FROM_TAG)
 export FROM = cbortz/$(FROM_OS):$(FROM_TAG)
 
-PUSH_TAGS = $(TAG) ruby-$(TAG)
+PUSH_TAGS = $(TAG)
 PUSH_REGISTRIES = $(REGISTRY)
 
 # And now, we source $(RUBY_TAG)/config.mk to get the shasum.
